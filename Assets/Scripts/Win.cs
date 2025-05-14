46303ed0b6cbf5b389bef39f7952451ebdb5c20a
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-05-14 13:33:12 +0300

    UI font + cursor

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -7,6 +7,8 @@ public class Win : MonoBehaviour
     {
         if (other.gameObject.tag == Globals.playerTag)
         {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
             SceneManager.LoadScene(Globals.winScene);
         }
     }

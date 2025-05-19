42c40f18947c8b715fd32371b59a59f0fe7f8681
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-05-20 00:05:08 +0300

    added babies on collision sounds

---
+++ Assets/BabyScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BabyScript : MonoBehaviour
+{
+    [Tooltip("Assign a baby sound AudioClip here")]
+    public AudioClip babyCryClip;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Grab (or add) the AudioSource on this GameObject
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the thing that hit us is tagged "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Play the cry clip once
+            if (babyCryClip != null)
+            {
+                audioSource.PlayOneShot(babyCryClip);
+            }
+            else
+            {
+                Debug.LogWarning("BabyScript: No babyCryClip assigned!", this);
+            }
+        }
+    }
+}
